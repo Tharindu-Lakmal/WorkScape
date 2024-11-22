9757ec6e6b7840d4632fc8772af7f480a71171c6
Tharindu-Lakmal <126395249+Tharindu-Lakmal@users.noreply.github.com>
2024-11-23 00:05:09 +0530

    custom CSS added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import BlogsHome from './pages/Blogs/BlogsHome'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
-    <div className="app">
+    <div className="app bg-[#0F172A] h-screen text-[#F0F4F8]">
+
+      <Navbar />
 
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/BlogHome' element={<BlogsHome />} />
       </Routes>
 
     </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,58 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'brightBlue': {
+        100: '#007BFF',
+        300: '#036de0',
+      },
+      'lightWhite': {
+        100: '#F0F4F8',
+        300: '#ffffff',
+      },
+      'bgDark': '#0F172A',
+    },
+    fontFamily: { // Custom fonts
+      sans1: ['Montserrat', 'sans-serif'],
+      sans2: ['Quicksand', 'sans-serif'],
+    },
+    extend: {
+      fontSize: { // Custom font sizes
+        small: '14px',
+        normal: '16px',
+        large: '32px',
+        extralarge: '64px',
+      },
+      spacing: { // Custom spacing
+        '4': '0.25rem',
+        '8': '0.5rem',
+        '12': '0.8rem',
+        '24': '1.5rem',
+        '32': '2rem',
+        '100': '6.25rem',
+        '120': '7.5rem',
+      },
+      letterSpacing: { // Custom letter spacing
+        normal: '0.6px',
+        medium: '0.25em', 
+      },
+      lineHeight: { // Custom line height | For example, if the font size is 16px and the line height is 1.5, the calculated line height is 16px × 1.5 = 24px.
+        small: '24px',
+        normal: '48px',
+        large: '96px',
+      },
+      borderRadius: {
+        small: '8px',
+        normal: '12px',
+        large: '32px',
+      }
+    },
   },
   plugins: [],
 }
-
